046052d716909fdbb58d457c6a8537a75b415445
Piotr Urbanczyk <p.m.urbanczyk@gmail.com>
2019-02-17 00:32:45 +0100

    Refactor

---
+++ src/leetCode/Easy/RobotReturntoOrigin.java
@@ -0,0 +1,25 @@
+package leetCode.Easy;
+
+public class RobotReturntoOrigin {
+	public static boolean judgeCircle(String moves) {
+		int ns = 0;
+		int we = 0;
+		for (char c : moves.toCharArray()) {
+			if (c == 'U') ns++;
+			if (c == 'D') ns--;
+			if (c == 'R') we++;
+			if (c == 'L') we--;
+			
+		}
+		System.out.println(ns);
+		System.out.println(we);
+		if (ns == 0 && we == 0) {
+			return true;
+		}
+		return false;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(judgeCircle("UDUDUULRLRRRLLL"));
+	}
+}
