78cd4075bf23f051fa3aea009d8d234fd577c6da
Piotr Urbanczyk <p.m.urbanczyk@gmail.com>
2019-02-19 00:00:06 +0100

    Hamming Distance added

---
+++ src/leetCode/Easy/HammingDistance.java
@@ -0,0 +1,26 @@
+package leetCode.Easy;
+
+public class HammingDistance {
+	public int hammingDistance(int x, int y) {
+		String xStr = Integer.toBinaryString(x);
+		String yStr = Integer.toBinaryString(y);
+		int ham = 0;
+		if (xStr.length() < yStr.length() ) {
+			while (xStr.length() < yStr.length()) {
+				xStr = "0" + xStr;
+			}
+		} else  {
+			while (xStr.length() > yStr.length()) {
+				yStr = "0" + yStr;
+			}
+		}
+		
+		for (int i = 0; i < xStr.length(); i++) {
+			if (xStr.charAt(i) != yStr.charAt(i)) {
+				ham++;
+			}
+		}
+		return ham;
+	}
+
+}
---
+++ src/leetCode/Easy/Main.java
@@ -0,0 +1,13 @@
+package leetCode.Easy;
+
+public class Main {
+
+	public static void main(String[] args) {
+		HammingDistance ham = new HammingDistance();
+		System.out.println(ham.hammingDistance(73, 93));
+		System.out.println(Integer.toBinaryString(1));
+		System.out.println(Integer.toBinaryString(4));
+
+	}
+
+}
