b4b1f0a28d21a2028e89a2d3bbcf3b1830a6b5e6
Piotr Urbanczyk <p.m.urbanczyk@gmail.com>
2019-02-19 00:14:03 +0100

    Delete Columns to Make Sorted added

--- src/leetCode/Easy/Main.java
+++ src/leetCode/Easy/Main.java
@@ -3,10 +3,9 @@ package leetCode.Easy;
 public class Main {
 
 	public static void main(String[] args) {
-		HammingDistance ham = new HammingDistance();
-		System.out.println(ham.hammingDistance(73, 93));
-		System.out.println(Integer.toBinaryString(1));
-		System.out.println(Integer.toBinaryString(4));
+		DeleteColumnsToMakeSorted del = new DeleteColumnsToMakeSorted();
+		String[] x = {"zyx","wvu","tsr"};
+		System.out.println(del.minDeletionSize(x));
 
 	}
 
