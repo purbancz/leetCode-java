8e579c4519dcd514750e3b8702be8fa865bf37ce
Piotr Urbanczyk <p.m.urbanczyk@gmail.com>
2019-04-07 00:39:48 +0200

    Number of Recent Calls added

--- src/leetCode/Easy/Main.java
+++ src/leetCode/Easy/Main.java
@@ -1,15 +1,25 @@
 package leetCode.Easy;
 
-import java.util.Arrays;
-import java.util.List;
-
 public class Main {
 
 	public static void main(String[] args) {
+
+		NumberOfRecentCalls obj = new NumberOfRecentCalls();
+		int param_1 = obj.pingrev(1);
+		int param_2 = obj.pingrev(100);
+		int param_3 = obj.pingrev(3001);
+		int param_4 = obj.pingrev(3002);
+		int param_5 = obj.pingrev(7002);
+		int param_6 = obj.pingrev(10001);
 		
-		SelfDividingNumbers selfDivid = new SelfDividingNumbers();
-		List<Integer> list = selfDivid.selfDividingNumbers(1, 22);
-		System.out.println(Arrays.toString(list.toArray()));
+		System.out.println(param_1);
+		System.out.println(param_2);
+		System.out.println(param_3);
+		System.out.println(param_4);
+		System.out.println(param_5);
+		System.out.println(param_6);
+		
+		
 
 	}
 
---
+++ src/leetCode/Easy/NumberOfRecentCalls.java
@@ -0,0 +1,30 @@
+package leetCode.Easy;
+
+import java.util.ArrayList;
+
+public class NumberOfRecentCalls {
+
+	ArrayList<Integer> calls;
+
+
+
+	public NumberOfRecentCalls() {
+		this.calls = new ArrayList<>();
+	}
+
+
+	public int pingrev(int t) {
+		calls.add(t);
+
+		int temp = 0;
+		for (int i = calls.indexOf(t); i >= 0; i--) {
+			if (calls.get(i) < t - 3000) {
+				temp = i + 1;
+				break;
+			}
+		}
+		return calls.indexOf(t) - temp + 1;
+
+	}
+
+}
