c090dfba9063deb92a93c9799a9e05a358af4496
Piotr Urbanczyk <p.m.urbanczyk@gmail.com>
2019-04-15 19:56:00 +0200

    Array Partition I added

--- src/leetCode/Easy/Main.java
+++ src/leetCode/Easy/Main.java
@@ -4,20 +4,12 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		NumberOfRecentCalls obj = new NumberOfRecentCalls();
-		int param_1 = obj.pingrev(1);
-		int param_2 = obj.pingrev(100);
-		int param_3 = obj.pingrev(3001);
-		int param_4 = obj.pingrev(3002);
-		int param_5 = obj.pingrev(7002);
-		int param_6 = obj.pingrev(10001);
+		ArrayPartitionI obj = new ArrayPartitionI();
+		int[] numbers = {1,2,3,4};
+		int result = obj.arrayPairSum(numbers);
+		System.out.println(result);
 		
-		System.out.println(param_1);
-		System.out.println(param_2);
-		System.out.println(param_3);
-		System.out.println(param_4);
-		System.out.println(param_5);
-		System.out.println(param_6);
+		
 		
 		
 
